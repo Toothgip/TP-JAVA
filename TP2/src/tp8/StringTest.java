a0f224fc6f5bb88b0b812a162509b524ec2b96ce
Thomas <thomas.broutier@gmail.com>
2016-05-23 10:24:43 +0200

    Ajout TP8 Supression du dossier TP7

---
+++ TP2/src/tp8/StringTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Thomas
+ */
+public class StringTest {
+    private static int choix;
+    private static Scanner input;
+    public static void main(String[] argv)
+    {
+        System.out.println("Welcome to the String Tester");
+        
+        choix = input.nextInt();
+        
+    }
+    
+}
